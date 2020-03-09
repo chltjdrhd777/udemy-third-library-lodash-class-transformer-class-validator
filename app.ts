932269a209dddb76622883e6c05f43405fcead86
chltjdrhd777 <58500558+chltjdrhd777@users.noreply.github.com>
2020-03-09 20:03:03 +0900

    ok

---
+++ app.ts
@@ -0,0 +1,3 @@
+import _ from "lodash"; // calling lodash
+
+console.log(_.shuffle([1, 2, 3])); // "_.shuffle()" is lodash syntax that allows me to make a randome array by using elements inside the array like [2,3,1] or [3,1,2]
